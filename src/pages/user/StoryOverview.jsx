a855f7354c223b8a78300bcdfafd9948ee7590c8
21120594 <21120594@student.hcmus.edu.vn>
2024-06-11 13:00:11 +0700

    Fix null author, loading overview

--- src/pages/user/StoryOverview.jsx
+++ src/pages/user/StoryOverview.jsx
@@ -11,6 +11,7 @@ import { ServerContext } from '../../context/ServerContext';
 import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { Pagination } from '../../components/Pagination'
+import { Spinner } from '@material-tailwind/react';
 
 export const StoryOverview = () => {
 	const { encodedUrl } = useParams();
@@ -22,12 +23,14 @@ export const StoryOverview = () => {
 	const [overviewService, setoverviewService] = useState([]);
 	const [check, setcheck] = useState('T');
 	const decodeUrl = decodeURIComponent(atob(encodedUrl));
+
 	const { server } = useContext(ServerContext);
 
 	const [chapterInforByPage, setChapterInfor] = useState([]);
 	const [booksReconmend, setBooks] = useState([]);
 	const [chapterInforByPage1, setChapterInfor1] = useState([]);
 
+	const [isLoading, setIsLoading] = useState(true);
 
 
 	useEffect(() => {
@@ -52,15 +55,19 @@ export const StoryOverview = () => {
 		getChapterInfor();
 		getChapterInfor1();
 
+		setIsLoading(true);
+		Promise.all([getRecommendation(), getOverviewParams(), getChapterInfor(), getChapterInfor1()]).then(() => setIsLoading(false));
 	}, []);
-
+	// if (overviewService.author) {
+	// 	console.log("Test overviews", overviewService.author.name);
+	// }
 	useEffect(() => {
 		const getUrlNewServer = async () => {
 			if (overviewService && Object.keys(overviewService).length > 0) {
 				const title = chuyenDoiKhongDau(overviewService.title.includes(' - ') ? overviewService.title.split(' - ')[0] : overviewService.title);
 				const data = await BookService.searchByName(title, 1);
 				if (data && data.length > 0) {
-					window.location.href = `/story/${btoa(data[0].url)}`;
+					window.location.href = `/story/${btoa(encodeURIComponent(data[0].url))}`;
 				}
 				else {
 					setcheck('F');
@@ -68,7 +75,8 @@ export const StoryOverview = () => {
 			}
 		}
 		getUrlNewServer();
-
+		setIsLoading(true);
+		Promise.all([getUrlNewServer()]).then(() => setIsLoading(false));
 	}, [server]);
 	useEffect(() => {
 		const getChapterInfor = async () => {
@@ -76,6 +84,8 @@ export const StoryOverview = () => {
 			if (data) setChapterInfor(data);
 		}
 		getChapterInfor();
+		setIsLoading(true);
+		Promise.all([getChapterInfor()]).then(() => setIsLoading(false));
 	}, [page]);
 
 	let temp1;
@@ -86,10 +96,9 @@ export const StoryOverview = () => {
 		}
 		temp1 = temp1.substring(temp1.lastIndexOf('/') + 1);
 	}
-	const encodeChap1 = btoa(temp1);
+	const encodeChap1 = btoa(encodeURIComponent(temp1));
 
 	const randomBooksReconmend = booksReconmend.slice(0, 5);
-
 
 	const breadcrumbItems = [
 		{ name: `${overviewService.title}`, link: `/story/${encodedUrl}` },
@@ -121,148 +130,155 @@ export const StoryOverview = () => {
 			className='mx-auto w-[1000px] mt-[20px]'
 		>
 			<Breadcrumb items={breadcrumbItems} />
-			{check == 'F' ? (
-				<div className=" font-bold text-center col-span-9 p-4  flex flex-col" >
-					Không tồn tại truyện tại server này !
-				</div>)
-				: (overviewService && Object.keys(overviewService).length > 0 &&
-					<div className="mx-auto container ">
-						<div className="grid grid-cols-12 gap-4 ">
-
-							< div className=" col-span-9 p-4  flex flex-col" >
-								<div className="grid grid-cols-4 gap-4  border-b border-gray-500 border-dashed">
-									<div className="col-span-1  rounded-md mb-2">
-										<img src={overviewService.coverImage}
-											alt="Title" className='w-[180px] h-[180px]' />
-									</div>
-									<div className="col-span-3  ">
-										<Typography className='text-2xl text-red-500 text-center'>{overviewService.title ? overviewService.title.toUpperCase() : "null"}
-										</Typography>
-										<div className="flex items-center justify-center pb-2">
-											<Typography className='text-sm text-center '>
-												Đánh giá <span style={{ fontStyle: 'italic', fontWeight: 'bold' }}>{overviewService.rating}/10</span>
-											</Typography>
-											<StarIcon className="h-6 w-6 text-yellow-500" />
-											<Typography className='text-sm text-center '>
-												từ <span style={{ fontStyle: 'italic', fontWeight: 'bold' }}>{overviewService.totalRating}</span> lượt
-											</Typography>
-										</div>
-										<div className="flex flex-row justify-center">
-											<button className="bg-blue-500 text-white font-bold py-2 px-4 rounded mr-2 hover:bg-gray-500" onClick={scrollToTarget}>
-												Danh sách chương
-											</button>
-											<button className="bg-blue-500 text-white font-bold py-2 px-4 rounded ml-2 hover:bg-gray-500" onClick={() => navigate(`/genre/${slug}`)}>
-												Truyện cùng loại
-											</button>
-										</div>
-
-										<div className="flex justify-center mt-2">
-
-											<button className="bg-red-500 text-white font-bold py-2 px-4 rounded hover:bg-gray-500" onClick={() => navigate(`/story/${encodedUrl}/${encodeChap1}`)}>
-												Đọc từ đầu
-											</button>
-										</div>
-									</div>
-								</div>
-
-								<div className="grid grid-cols-4 gap-4  ">
-									<div className="col-span-1  mb-2 border-r border-gray-500 border-dashed ">
-										<div className="flex items-center mt-2">
-											<UserIcon className="h-6 w-6 flex-shrink-0" />
-											<Typography className='text-sl text-dark-500 ml-2'>{overviewService.author}</Typography>
-										</div>
-										<div className="flex items-center mt-2">
-											<Square2StackIcon className="h-6 w-6 flex-shrink-0" />
-											<Typography className='text-sl text-dark-500 ml-2'>{overviewService.status}</Typography>
-										</div>
-										<div className="flex items-center mt-2">
-											<TagIcon className="h-6 w-6 flex-shrink-0" />
-											<Typography className='text-sl text-dark-500 ml-2'>{overviewService.genre}</Typography>
-										</div>
-										<div className="flex items-center mt-2">
-											<EyeIcon className="h-6 w-6 flex-shrink-0" />
-											<Typography className='text-sl text-dark-500 ml-2'>{overviewService.totalViews === 0 ? "null" : overviewService.totalViews} </Typography>
-										</div>
-										<div className="flex items-center mt-2">
-											<ArrowPathIcon className="h-6 w-6 flex-shrink-0" />
-											<Typography className='text-sl text-dark-500 ml-2'>{overviewService.updatedDate ? new Date(overviewService.updatedDate).toLocaleString() : 'null'}</Typography>
-										</div>
-									</div>
-
-									<div className="col-span-3   ">
-										<p className="text-sl">
-											{overviewService.description}
-										</p>
-									</div>
-								</div>
-							</div>
-
-							<div className="col-span-3  p-4 h-[550px]">
-								<Typography
-									className='text-[#2f52b2] text-lg font-semibold'
-								>
-									TRUYỆN ĐỀ CỬ
-								</Typography>
-								{
-									randomBooksReconmend.map((book, i) => {
-										const newEncodedUrl = btoa(book.url);
-										return (
-											<div key={i} className="items-center pt-2 pb-2 border-t border-gray-500 flex flex-row" >
-												<img src={book.coverImage}
-													alt="Image" className='w-20 h-20 flex-shrink-0' />
-												<div className="flex flex-col ml-2">
-													<Link to={`/story/${newEncodedUrl}`} className="hover:underline">
-														<Typography className='text-sm' onClick={() => window.location.href = `/story/${newEncodedUrl}`}>
-															<i>{book.title ? book.title : "null"}</i>
-														</Typography>
-													</Link>
-													<Typography className='text-[#2C7ABE] text-sm'>
-														<i>{book.author}</i>
-													</Typography>
-
-												</div>
-											</div>)
-									})
-								}
-							</div>
-						</div>
-
-						<div ref={targetRef} className="grid grid-cols-12 gap-4 mb-10 ">
-
-							<div className=" col-span-9  flex flex-col " >
-								<Typography
-									className='text-[#2f52b2] text-lg font-semibold pl-2'
-								>
-									CHƯƠNG MỚI NHẤT
-								</Typography>
-								{
-									chapterInforByPage.map((chap, i) => {
-										let temp = chap.url;
-										if (temp.endsWith('/')) {
-											temp = temp.slice(0, -1);
-										}
-										temp = temp.substring(temp.lastIndexOf('/') + 1);
-										const encodeChap = btoa(temp);
-										return (
-											<div key={i} className="border-t border-gray-500 pt-1 pb-1 hover:bg-gray-500" onClick={() => navigate(`/story/${encodedUrl}/${encodeChap}`)}>
-												<Typography
-													className='text-sm cursor-pointer '
-												>
-													{chap.title ? chap.title : "null"}
-												</Typography>
-											</div>
-										)
-									})
-
-								}
-
-								<CardFooter className='mx-auto -mt-4'>
-									<Pagination pageLimit={overviewService.maxPageOfChapter} />
-								</CardFooter>
-
-							</div>
-						</div>
+			{
+				isLoading ? (
+					<div className="flex items-center justify-center h-screen">
+						<Spinner />
 					</div>
+				) : (
+					check == 'F' ? (
+						<div className=" font-bold text-center col-span-9 p-4  flex flex-col" >
+							Không tồn tại truyện tại server này !
+						</div>)
+						: (overviewService && Object.keys(overviewService).length > 0 &&
+							<div className="mx-auto container ">
+								<div className="grid grid-cols-12 gap-4 ">
+
+									< div className=" col-span-9 p-4  flex flex-col" >
+										<div className="grid grid-cols-4 gap-4  border-b border-gray-500 border-dashed">
+											<div className="col-span-1  rounded-md mb-2">
+												<img src={overviewService.coverImage}
+													alt="Title" className='w-[180px] h-[180px]' />
+											</div>
+											<div className="col-span-3  ">
+												<Typography className='text-2xl text-red-500 text-center'>{overviewService.title ? overviewService.title.toUpperCase() : "null"}
+												</Typography>
+												<div className="flex items-center justify-center pb-2">
+													<Typography className='text-sm text-center '>
+														Đánh giá <span style={{ fontStyle: 'italic', fontWeight: 'bold' }}>{overviewService.rating}/10</span>
+													</Typography>
+													<StarIcon className="h-6 w-6 text-yellow-500" />
+													<Typography className='text-sm text-center '>
+														từ <span style={{ fontStyle: 'italic', fontWeight: 'bold' }}>{overviewService.totalRating}</span> lượt
+													</Typography>
+												</div>
+												<div className="flex flex-row justify-center">
+													<button className="bg-blue-500 text-white font-bold py-2 px-4 rounded mr-2 hover:bg-gray-500" onClick={scrollToTarget}>
+														Danh sách chương
+													</button>
+													<button className="bg-blue-500 text-white font-bold py-2 px-4 rounded ml-2 hover:bg-gray-500" onClick={() => navigate(`/genre/${slug}`)}>
+														Truyện cùng loại
+													</button>
+												</div>
+
+												<div className="flex justify-center mt-2">
+
+													<button className="bg-red-500 text-white font-bold py-2 px-4 rounded hover:bg-gray-500" onClick={() => navigate(`/story/${encodedUrl}/${encodeChap1}`)}>
+														Đọc từ đầu
+													</button>
+												</div>
+											</div>
+										</div>
+
+										<div className="grid grid-cols-4 gap-4  ">
+											<div className="col-span-1  mb-2 border-r border-gray-500 border-dashed ">
+												<div className="flex items-center mt-2">
+													<UserIcon className="h-6 w-6 flex-shrink-0" />
+													<Typography className='text-sl text-dark-500 ml-2'>{overviewService.author ? overviewService.author.name : "null"}</Typography>
+												</div>
+												<div className="flex items-center mt-2">
+													<Square2StackIcon className="h-6 w-6 flex-shrink-0" />
+													<Typography className='text-sl text-dark-500 ml-2'>{overviewService.status}</Typography>
+												</div>
+												<div className="flex items-center mt-2">
+													<TagIcon className="h-6 w-6 flex-shrink-0" />
+													<Typography className='text-sl text-dark-500 ml-2'>{overviewService.genre}</Typography>
+												</div>
+												<div className="flex items-center mt-2">
+													<EyeIcon className="h-6 w-6 flex-shrink-0" />
+													<Typography className='text-sl text-dark-500 ml-2'>{overviewService.totalViews === 0 ? "null" : overviewService.totalViews} </Typography>
+												</div>
+												<div className="flex items-center mt-2">
+													<ArrowPathIcon className="h-6 w-6 flex-shrink-0" />
+													<Typography className='text-sl text-dark-500 ml-2'>{overviewService.updatedDate ? new Date(overviewService.updatedDate).toLocaleString() : 'null'}</Typography>
+												</div>
+											</div>
+
+											<div className="col-span-3   ">
+												<p className="text-sl">
+													{overviewService.description}
+												</p>
+											</div>
+										</div>
+									</div>
+
+									<div className="col-span-3  p-4 h-[550px]">
+										<Typography
+											className='text-[#2f52b2] text-lg font-semibold'
+										>
+											TRUYỆN ĐỀ CỬ
+										</Typography>
+										{
+											randomBooksReconmend.map((book, i) => {
+												const newEncodedUrl = btoa(encodeURIComponent(book.url));
+												return (
+													<div key={i} className="items-center pt-2 pb-2 border-t border-gray-500 flex flex-row" >
+														<img src={book.coverImage}
+															alt="Image" className='w-20 h-20 flex-shrink-0' />
+														<div className="flex flex-col ml-2">
+															<Link to={`/story/${newEncodedUrl}`} className="hover:underline">
+																<Typography className='text-sm' onClick={() => window.location.href = `/story/${newEncodedUrl}`}>
+																	<i>{book.title ? book.title : "null"}</i>
+																</Typography>
+															</Link>
+															<Typography className='text-[#2C7ABE] text-sm'>
+																<i>{book.author ? book.author.name : "null"}</i>
+															</Typography>
+
+														</div>
+													</div>)
+											})
+										}
+									</div>
+								</div>
+
+								<div ref={targetRef} className="grid grid-cols-12 gap-4 mb-10 ">
+
+									<div className=" col-span-9  flex flex-col " >
+										<Typography
+											className='text-[#2f52b2] text-lg font-semibold pl-2'
+										>
+											CHƯƠNG MỚI NHẤT
+										</Typography>
+										{
+											chapterInforByPage.map((chap, i) => {
+												let temp = chap.url;
+												if (temp.endsWith('/')) {
+													temp = temp.slice(0, -1);
+												}
+												temp = temp.substring(temp.lastIndexOf('/') + 1);
+												const encodeChap = btoa(encodeURIComponent(temp));
+												return (
+													<div key={i} className="border-t border-gray-500 pt-1 pb-1 hover:bg-gray-500" onClick={() => navigate(`/story/${encodedUrl}/${encodeChap}`)}>
+														<Typography
+															className='text-sm cursor-pointer '
+														>
+															{chap.title ? chap.title : "null"}
+														</Typography>
+													</div>
+												)
+											})
+
+										}
+
+										<CardFooter className='mx-auto -mt-4'>
+											<Pagination pageLimit={overviewService.maxPageOfChapter} />
+										</CardFooter>
+
+									</div>
+								</div>
+							</div>
+						)
 				)}
 		</div >
 	)
