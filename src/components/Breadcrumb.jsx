952560a9f21a5e33bb9dc91dc6bfea9dc12d1028
21120594 <21120594@student.hcmus.edu.vn>
2024-06-20 17:21:03 +0700

    Fix Breadcrumb

--- src/components/Breadcrumb.jsx
+++ src/components/Breadcrumb.jsx
@@ -1,31 +1,20 @@
 import { HomeIcon } from '@heroicons/react/24/solid';
 import { Link } from 'react-router-dom';
-import {Typography} from '@material-tailwind/react';
+import { Typography } from '@material-tailwind/react';
 export function Breadcrumb({ items }) {
 	return (
 		<div className="breadcrumb h-[40px] text-sm flex items-center">
-			<Link to="/">
-				<div className="flex items-center">
-					<HomeIcon className='h-5 w-5 p-1'/>
-					<Typography
-						as="h5"
-					>
-						Trang chủ
-					</Typography>
-				</div>
+			<Link to="/" className="flex items-center flex-shrink-0">
+				<HomeIcon className="h-5 w-5 p-1 flex-shrink-0" />
+				<Typography as="h5" className="flex-shrink-0">
+					Trang chủ
+				</Typography>
 			</Link>
 			{items.map((item, index) => (
-				<span key={index} className='flex items-center'>
-
-					<span
-						className='p-1'
-					>
-						&gt;
-					</span>
-					<Link to={item.link} >
-						<Typography
-							as="h5"
-						>
+				<span key={index} className="flex items-center flex-shrink-0">
+					<span className="p-1 flex-shrink-0">&gt;</span>
+					<Link to={item.link} className="flex-shrink-0">
+						<Typography as="h5" className="flex-shrink-0">
 							{item.name}
 						</Typography>
 					</Link>
